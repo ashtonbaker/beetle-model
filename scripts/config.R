79d12fc1c339fd674a3bdeb53356f38a5d0f5207
Ashton Baker <ashtonsb@umich.edu>
2017-01-20 11:06:34 -0500

    first commit

---
+++ scripts/config.R
@@ -0,0 +1,20 @@
+opt.stages.E <- 7
+opt.stages.L <- 7
+opt.stages.P <- 7
+opt.stages.A <- 1
+
+opt.initial-pfilter.np <- 1000
+opt.initial-pfilter.i_max <- 10
+
+opt.local-box-search.np <- 1000
+opt.local-box-search.nmif <- 1000
+opt.local-box-search.i_max <- 20
+opt.local-box-search.rw.sd <- c(b=0.02, cea=0.02, cel=0.02, cpa=0.02, mu_A=0.02, mu_L=0.02, od=0.02, tau_E=0.02, tau_L=0.02, tau_P=0.02)
+
+opt.lik-local.nrep <- 10
+opt.lik-local.np <- 1000
+
+opt.global-search.nguesses <- 1000
+opt.global-search.nmif <- 30
+opt.global-search.nrep <- 10
+opt.global-search.np <- 1000
