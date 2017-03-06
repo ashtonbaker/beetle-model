996bf6741b9b97a358c583e198afcf9ba297d4a7
Ashton Baker <ashtonsb@umich.edu>
2017-03-05 21:51:09 -0500

    standardized some spacing

--- scripts/panel.R
+++ scripts/panel.R
@@ -56,77 +56,79 @@ pompList <- setNames(object = vector(mode = "list", length = U),
                      nm = paste0("unit", 1:U))
 
 for (i.u in 1:U) {
-  glob_snippet <- Csnippet(sprintf("
-#include <math.h>
-#define ESTAGES %d
-#define LSTAGES %d
-#define PSTAGES %d
-#define ASTAGES %d
-#define L_0 250
-#define P_0 5
-#define A_0 100
-",
-                                   opt.stages.E,
-                                   opt.stages.L,
-                                   opt.stages.P,
-                                   opt.stages.A))
+  glob_snippet <-
+    Csnippet(sprintf("
+      #include <math.h>
+      #define ESTAGES %d
+      #define LSTAGES %d
+      #define PSTAGES %d
+      #define ASTAGES %d
+      #define L_0 250
+      #define P_0 5
+      #define A_0 100
+      ",
+      opt.stages.E,
+      opt.stages.L,
+      opt.stages.P,
+      opt.stages.A))
 
-init_snippet <- Csnippet("
-  double *E = &E1;
-  double *L = &L1;
-  double *P = &P1;
+  init_snippet <-
+    Csnippet("
+      double *E = &E1;
+      double *L = &L1;
+      double *P = &P1;
 
-  double gamma_L = (LSTAGES / tau_L) * (1 - mu_L);
-  double gamma_P = (PSTAGES / tau_P) * exp((-cpa * A_0) / ESTAGES);
+      double gamma_L = (LSTAGES / tau_L) * (1 - mu_L);
+      double gamma_P = (PSTAGES / tau_P) * exp((-cpa * A_0) / ESTAGES);
 
-  double mu_l = (LSTAGES / tau_L) * mu_L;
-  double mu_p = (PSTAGES / tau_P) * (1 - exp((-cpa * A_0) / ESTAGES));
+      double mu_l = (LSTAGES / tau_L) * mu_L;
+      double mu_p = (PSTAGES / tau_P) * (1 - exp((-cpa * A_0) / ESTAGES));
 
-  double L_rate[LSTAGES] = {0};
-  double P_rate[PSTAGES] = {0};
+      double L_rate[LSTAGES] = {0};
+      double P_rate[PSTAGES] = {0};
 
-  int k;
-  double sum;
-  for (k = 0, sum = 0; k < LSTAGES; k++){
-     L_rate[k] = pow(gamma_L/(gamma_L + mu_l), k);
-     sum += L_rate[k];
-  }
-  for (k = 0; k < LSTAGES; k++) L_rate[k] /= sum;
-  for (k = LSTAGES - 1, sum = 0; k >=0; k--){
-    sum += L_rate[k];
-    L_rate[k] /= sum;
-  }
+      int k;
+      double sum;
+      for (k = 0, sum = 0; k < LSTAGES; k++){
+        L_rate[k] = pow(gamma_L/(gamma_L + mu_l), k);
+        sum += L_rate[k];
+      }
+      for (k = 0; k < LSTAGES; k++) L_rate[k] /= sum;
+      for (k = LSTAGES - 1, sum = 0; k >=0; k--){
+        sum += L_rate[k];
+        L_rate[k] /= sum;
+      }
 
-  for (k = 0, sum = 0; k < PSTAGES; k++){
-    P_rate[k] = pow(gamma_P/(gamma_P + mu_p), k);
-    sum += P_rate[k];
-  }
+      for (k = 0, sum = 0; k < PSTAGES; k++){
+        P_rate[k] = pow(gamma_P/(gamma_P + mu_p), k);
+        sum += P_rate[k];
+      }
 
-  for (k = 0; k < PSTAGES; k++) P_rate[k] /= sum;
-  for (k = PSTAGES - 1, sum = 0; k >=0; k--){
-    sum += P_rate[k];
-    P_rate[k] /= sum;
-  }
+      for (k = 0; k < PSTAGES; k++) P_rate[k] /= sum;
+      for (k = PSTAGES - 1, sum = 0; k >=0; k--){
+        sum += P_rate[k];
+        P_rate[k] /= sum;
+      }
 
 
-  for (k = 0; k < ESTAGES; k++) E[k] = 0;
+      for (k = 0; k < ESTAGES; k++) E[k] = 0;
 
-  int L_count = L_0;
-  for (k = 0; k < LSTAGES - 1; k++){
-    L[k] = rbinom(L_count, L_rate[k]);
-    L_count -= L[k];
-  }
-  L[LSTAGES - 1] = L_count;
+      int L_count = L_0;
+      for (k = 0; k < LSTAGES - 1; k++){
+        L[k] = rbinom(L_count, L_rate[k]);
+        L_count -= L[k];
+      }
+      L[LSTAGES - 1] = L_count;
 
-  int P_count = P_0;
-  for (k = 0; k < PSTAGES - 1; k++){
-    P[k] = rbinom(P_count, P_rate[k]);
-    P_count -= P[k];
-  }
+      int P_count = P_0;
+      for (k = 0; k < PSTAGES - 1; k++){
+        P[k] = rbinom(P_count, P_rate[k]);
+        P_count -= P[k];
+      }
 
-  P[PSTAGES - 1] = P_count;
+      P[PSTAGES - 1] = P_count;
 
-  A = 100;")
+      A = 100;")
 
   rproc_snippet <-
     Csnippet(sprintf("
@@ -218,38 +220,37 @@ init_snippet <- Csnippet("
         }
         P_prev = P_tot;
         A_prev = A;
+      }",
+      mu_A[i.u], cpa[i.u]))
+
+  dmeas_snippet <-
+    Csnippet("
+      const double *L = &L1;
+      const double *P = &P1;
+      double fudge = 1e-9;
+
+      int k;
+      double L_tot = 0;
+      double P_tot = 0;
+      for (k = 0; k < LSTAGES; k++) L_tot += L[k];
+      for (k = 0; k < PSTAGES; k++) P_tot += P[k];
+
+      lik = dnbinom_mu(L_obs, 1/od, L_tot + fudge, 1) +
+            dnbinom_mu(P_obs, 1/od, P_tot + fudge, 1) +
+            dnbinom_mu(A_obs, 1/od, A + fudge,     1);
+
+      if(!R_FINITE(lik)){
+        Rprintf(\"\\n\\nweeks %f\", t);
+        Rprintf(\"\\nL_tot %f\", L_tot);
+        Rprintf(\"\\nP_tot %f\", P_tot);
+        Rprintf(\"\\nA_tot %f\", A);
+        Rprintf(\"\\nL_obs %f\", L_obs);
+        Rprintf(\"\\nP_obs %f\", P_obs);
+        Rprintf(\"\\nA_obs %f\", A_obs);
+        Rprintf(\"\\nloglik %f\",lik);
       }
-      ", mu_A[i.u], cpa[i.u]))
 
-  dmeas_snippet <- Csnippet(
-    "
-    const double *L = &L1;
-    const double *P = &P1;
-    double fudge = 1e-9;
-
-    int k;
-    double L_tot = 0;
-    double P_tot = 0;
-    for (k = 0; k < LSTAGES; k++) L_tot += L[k];
-    for (k = 0; k < PSTAGES; k++) P_tot += P[k];
-
-    lik = dnbinom_mu(L_obs, 1/od, L_tot + fudge, 1) +
-          dnbinom_mu(P_obs, 1/od, P_tot + fudge, 1) +
-          dnbinom_mu(A_obs, 1/od, A + fudge,     1);
-
-    if(!R_FINITE(lik)){
-      Rprintf(\"\\n\\nweeks %f\", t);
-      Rprintf(\"\\nL_tot %f\", L_tot);
-      Rprintf(\"\\nP_tot %f\", P_tot);
-      Rprintf(\"\\nA_tot %f\", A);
-      Rprintf(\"\\nL_obs %f\", L_obs);
-      Rprintf(\"\\nP_obs %f\", P_obs);
-      Rprintf(\"\\nA_obs %f\", A_obs);
-      Rprintf(\"\\nloglik %f\",lik);
-    }
-
-    lik = (give_log) ? lik : exp(lik);
-     ")
+      lik = (give_log) ? lik : exp(lik);")
 
   rmeas_snippet <-
     Csnippet("
@@ -291,7 +292,8 @@ init_snippet <- Csnippet("
       Ttau_E = log(tau_E-ESTAGES);
       Ttau_L = log(tau_L-LSTAGES);
       Ttau_P = log(tau_P-PSTAGES);
-      Tod = log(od);")
+      Tod = log(od);
+      ")
 
 # CONSTRUCTION OF PANEL POMP OBJECT ############################################
 
@@ -461,8 +463,8 @@ results_global <-
       specific_params <- default_coef$specific
 
       mf <-
-	mif2(
-	  mf1,
+        mif2(
+          mf1,
           shared.start=c(unlist(guess)),
           specific.start=specific_params,
           tol=1e-180,
@@ -471,7 +473,9 @@ results_global <-
         replicate(
           opt.global.search.nrep,
           logLik(pfilter(mf,Np=opt.global.search.np)))
+
       ll <- logmeanexp(ll,se=TRUE)
+
       c(coef(mf)$shared,loglik=ll[1],loglik=ll[2])
     }
   })
